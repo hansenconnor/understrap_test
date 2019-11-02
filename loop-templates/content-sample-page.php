7c7e093729d16e6ca881e344fdb807d02698a094
hanse174 <hanse174@mail.nmc.edu>
2019-11-01 22:19:21 -0400

    Initial commit

---
+++ loop-templates/content-sample-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Sample Page content
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+?>
+
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+
+			</div>
+		</div>
+	</div>
+</section>
